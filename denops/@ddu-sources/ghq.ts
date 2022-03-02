af22e3f3a81a3377cacfa44170198ead7ed41f72
4513ECHO <4513echo@gmail.com>
2022-03-02 22:30:19 +0900

    🔨 refactor: Refactor codes
    - Update deps
    - split functioin

--- denops/@ddu-sources/ghq.ts
+++ denops/@ddu-sources/ghq.ts
@@ -1,38 +1,39 @@
-import * as fn from "https://deno.land/x/denops_std@v3.0.0/function/mod.ts";
+import * as fn from "https://deno.land/x/denops_std@v3.1.4/function/mod.ts";
 import type { ActionData } from "https://deno.land/x/ddu_kind_file@v0.2.0/file.ts";
-import type { GatherArguments } from "https://deno.land/x/ddu_vim@v0.12.2/base/source.ts";
-import type { Item } from "https://deno.land/x/ddu_vim@v0.12.2/types.ts";
-import { BaseSource } from "https://deno.land/x/ddu_vim@v0.12.2/types.ts";
-import { ensureString } from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
-import { basename } from "https://deno.land/std@0.125.0/path/mod.ts";
+import type { GatherArguments } from "https://deno.land/x/ddu_vim@v1.1.0/base/source.ts";
+import type { Item } from "https://deno.land/x/ddu_vim@v1.1.0/types.ts";
+import { BaseSource } from "https://deno.land/x/ddu_vim@v1.1.0/types.ts";
+import { ensureString } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
+import { basename } from "https://deno.land/std@0.127.0/path/mod.ts";
 
 interface Params {
   bin: string;
   display: "raw" | "basename" | "shorten";
 }
 
+async function displayWord(
+  args: GatherArguments<Params>,
+  item: string,
+): Promise<string> {
+  if (args.sourceParams.display === "raw") {
+    return item;
+  } else if (args.sourceParams.display === "basename") {
+    return basename(item);
+  } else if (args.sourceParams.display === "shorten") {
+    return ensureString(await fn.pathshorten(args.denops, item));
+  } else {
+    await args.denops.call(
+      "ddu#util#print_error",
+      `Invalid display param: ${args.sourceParams.display}`,
+    );
+    return;
+  }
+}
+
 export class Source extends BaseSource<Params, ActionData> {
   kind = "file";
 
   gather(args: GatherArguments<Params>): ReadableStream<Item<ActionData>[]> {
-    const displayWord = async (item: string): Promise<string> => {
-      if (args.sourceParams.display == "raw") {
-        return item;
-      } else if (args.sourceParams.display === "basename") {
-        return basename(item);
-      } else if (args.sourceParams.display === "shorten") {
-        const result = await fn.pathshorten(args.denops, item);
-        ensureString(result);
-        return result;
-      } else {
-        await args.denops.call(
-          "ddu#util#print_error",
-          `Invalid display param: ${args.sourceParams.display}`,
-        );
-        return;
-      }
-    };
-
     return new ReadableStream({
       async start(controller) {
         const proc = Deno.run({
@@ -52,7 +53,7 @@ export class Source extends BaseSource<Params, ActionData> {
         proc.close();
         controller.enqueue(
           await Promise.all(paths.map(async (i) => {
-            const display = await displayWord(i);
+            const display = await displayWord(args, i);
             return {
               word: i,
               display: display,
